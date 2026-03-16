8f4f311418ad9f5cf83bd960462c70498564af5a
VANGIMALLABALASIVAPRASADREDDY <balasiva9502212095@gmail.com>
2026-03-16 09:20:56 +0530

    Add files via upload

---
+++ Lab Experiments/exp-30.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("shoe_sales.csv")
+
+freq = data.groupby("shoe_size")["quantity"].sum()
+
+print("Frequency Distribution of Shoe Sizes")
+print(freq)
+
+freq.plot(kind="bar")
+
+plt.xlabel("Shoe Size")
+plt.ylabel("Quantity Sold")
+plt.title("Frequency Distribution of Shoe Sizes")
+
+plt.show()
